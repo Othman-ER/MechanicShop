1764d4904176c9dbcda0fca8463748bcc7fadef5
Othman <erothman15er@gmail.com>
2026-03-10 20:40:23 +0000

    Upgrade solution to .NET 10 and optimize dependency management (#1)
    * chore: successful migration to .NET 10 and cleanup of duplicate dependencies
    
    * chore: upgrade solution to .NET 10
    
    ---------
    
    Co-authored-by: Othman er rouydy <erothman15er@mail.com>

--- Tests/MechanicShop.Domain.UnitTests/obj/Debug/net9.0/MechanicShop.Domain.UnitTests.AssemblyInfo.cs
+++ Tests/MechanicShop.Domain.UnitTests/obj/Debug/net9.0/MechanicShop.Domain.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicShop.Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34480737a6e3a43dc81a817466a22a920655b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicShop.Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicShop.Domain.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/MechanicShop.Api/obj/Debug/net9.0/MechanicShop.Api.AssemblyInfo.cs
+++ src/MechanicShop.Api/obj/Debug/net9.0/MechanicShop.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MechanicShop.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34480737a6e3a43dc81a817466a22a920655b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("MechanicShop.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MechanicShop.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
